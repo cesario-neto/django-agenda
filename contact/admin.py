c0fae28a85be80fb94db6395a4c43c5898ccec6f
Cesario_Neto <cesarioneto7@gmail.com>
2023-09-05 10:00:02 -0300

    Customizing admin options

--- contact/admin.py
+++ contact/admin.py
@@ -4,4 +4,11 @@ from . import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    ...
+    list_display = 'first_name', 'last_name', 'phone',
+    ordering = '-id',
+    list_filter = 'created_date',
+    search_fields = 'first_name', 'last_name',
+    list_editable = 'phone',
+    list_per_page = 20
+    list_max_show_all = 100
+    list_display_links = 'first_name', 'last_name',
