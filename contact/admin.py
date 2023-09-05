c12f8c12c384236e46b52ab72d27f6fffb4b9bcd
Cesario_Neto <cesarioneto7@gmail.com>
2023-09-05 11:16:05 -0300

    Creating model category and registering in admin

--- contact/admin.py
+++ contact/admin.py
@@ -2,13 +2,21 @@ from django.contrib import admin
 from . import models
 
 
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    search_fields = 'name',
+    ordering = '-id',
+    list_per_page = 20
+    list_max_show_all = 100
+
+
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = 'first_name', 'last_name', 'phone',
+    list_display = 'first_name', 'last_name', 'phone', 'category',
     ordering = '-id',
-    list_filter = 'created_date',
+    list_filter = 'created_date', 'category',
     search_fields = 'first_name', 'last_name',
-    list_editable = 'phone',
+    list_editable = 'phone', 'category',
     list_per_page = 20
     list_max_show_all = 100
     list_display_links = 'first_name', 'last_name',
