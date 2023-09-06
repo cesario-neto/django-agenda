431c7ecbefef1cc7b2bcd1477d08870f8abdfe96
Cesario_Neto <cesarioneto7@gmail.com>
2023-09-06 14:00:58 -0300

    Creating a view to search contacts

--- contact/views/contact_view.py
+++ contact/views/contact_view.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from contact.models import Contact
+from django.db.models import Q
 
 
 def index(request):
@@ -13,11 +14,33 @@ def index(request):
     return render(request, 'contact/index.html', context)
 
 
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+
+    if search_value == '':
+        return redirect('contact:index')
+
+    contacts = Contact.objects.filter(show=True).filter(
+        Q(first_name__icontains=search_value) |
+        Q(last_name__icontains=search_value) |
+        Q(email__icontains=search_value) |
+        Q(phone__icontains=search_value)
+    ).order_by('-id')
+
+    context = {
+        'contacts': contacts,
+        'site_title': 'Contato -',
+    }
+
+    return render(request, 'contact/index.html', context)
+
+
 def contact(request, contact_id):
     single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
 
+    contact_title = f'{single_contact.first_name} {single_contact.last_name}'
     context = {
         'contact': single_contact,
-        'site_title': f'{single_contact.first_name} {single_contact.last_name} -',
+        'site_title': f'{contact_title} -',
     }
     return render(request, 'contact/contact.html', context)
