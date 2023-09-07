b08b6a6721ecd175d2cb1f241df811ad17a29d7e
Cesario_Neto <cesarioneto7@gmail.com>
2023-09-07 16:48:15 -0300

    Creating and displaying contact form

---
+++ contact/forms.py
@@ -0,0 +1,25 @@
+from django.core.exceptions import ValidationError
+from django import forms
+from .models import Contact
+
+
+class ContactForms(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = ('first_name', 'last_name', 'phone',
+                  'email', 'description', 'category')
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
+
+        if first_name == last_name:
+            error_msg = ValidationError(
+                'Primeiro nome não pode ser igual ao sobrenome',
+                code='invalid'
+            )
+            self.add_error('first_name', error_msg)
+            self.add_error('last_name', error_msg)
+
+        return super().clean()
--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
+from contact.forms import ContactForms
 
 
 def create(request):
+    form = ContactForms()
+
+    if request.method == 'POST':
+        form = ContactForms(request.POST)
 
     context = {
-
+        'form': form
     }
 
     return render(request, 'contact/create.html', context)
