6fd05030d983a7f53f35525bbba3e43e420be62c
Cesario_Neto <cesarioneto7@gmail.com>
2023-09-07 23:15:34 -0300

    Creating view to update

--- contact/urls.py
+++ contact/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
 
     # CRUD
     path('contact/create', views.create, name='create'),
+    path('contact/<int:contact_id>/update', views.update, name='update'),
     path('contact/<int:contact_id>/', views.contact, name='contact'),
 ]
--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,19 +1,43 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from contact.forms import ContactForms
+from django.urls import reverse
+from contact.models import Contact
 
 
 def create(request):
     form = ContactForms()
+    form_url = reverse('contact:create')
 
     if request.method == 'POST':
         form = ContactForms(request.POST)
 
         if form.is_valid():
-            form.save()
-            return redirect('contact:create')
+            contact_form = form.save()
+            return redirect('contact:update', contact_id=contact_form.pk)
 
     context = {
-        'form': form
+        'form': form,
+        'form_url': form_url
+    }
+
+    return render(request, 'contact/create.html', context)
+
+
+def update(request, contact_id):
+    contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    form_url = reverse('contact:update', args=(contact_id,))
+    form = ContactForms(instance=contact)
+
+    if request.method == 'POST':
+        form = ContactForms(request.POST, instance=contact)
+
+        if form.is_valid():
+            contact_form = form.save()
+            return redirect('contact:update', contact_id=contact_form.pk)
+
+    context = {
+        'form': form,
+        'form_url': form_url
     }
 
     return render(request, 'contact/create.html', context)
