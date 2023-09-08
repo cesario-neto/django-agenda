81db78ea0aeda4602211c81a8702e72e11ca25a7
Cesario_Neto <cesarioneto7@gmail.com>
2023-09-08 10:25:51 -0300

    Creating view to delete contact

--- contact/urls.py
+++ contact/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     # CRUD
     path('contact/create', views.create, name='create'),
     path('contact/<int:contact_id>/update', views.update, name='update'),
+    path('contact/<int:contact_id>/delete', views.delete, name='delete'),
     path('contact/<int:contact_id>/', views.contact, name='contact'),
 ]
