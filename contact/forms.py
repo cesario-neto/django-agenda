5263914a2c373d3e4cb0c24ccd45ac0a621e2900
Cesario_Neto <cesarioneto7@gmail.com>
2023-09-09 11:58:30 -0300

    Creating form and view for registration

--- contact/forms.py
+++ contact/forms.py
@@ -1,6 +1,8 @@
 from django.core.exceptions import ValidationError
 from django import forms
 from .models import Contact
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 
 class ContactForms(forms.ModelForm):
@@ -32,3 +34,33 @@ class ContactForms(forms.ModelForm):
             self.add_error('last_name', error_msg)
 
         return super().clean()
+
+
+class RegisterForm(UserCreationForm):
+    first_name = forms.CharField(
+        required=True,
+        min_length=3,
+    )
+
+    last_name = forms.CharField(
+        required=True,
+        min_length=3,
+    )
+
+    email = forms.EmailField(
+        required=True,
+    )
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'username',
+                  'email', 'password1', 'password2')
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        if User.objects.filter(email=email).exists():
+            self.add_error(
+                'email',
+                ValidationError('Email já cadastrado', code='invalid')
+            )
--- contact/urls.py
+++ contact/urls.py
@@ -12,4 +12,7 @@ urlpatterns = [
     path('contact/<int:contact_id>/update', views.update, name='update'),
     path('contact/<int:contact_id>/delete', views.delete, name='delete'),
     path('contact/<int:contact_id>/', views.contact, name='contact'),
+
+    # User
+    path('user/create/', views.register, name='register')
 ]
