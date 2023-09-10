261b3e0f58fc9df407780d6348f903d0e5fe0fc9
Cesario_Neto <cesarioneto7@gmail.com>
2023-09-10 18:37:03 -0300

    Creating login and logout view

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse
 from contact.forms import RegisterForm
 from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate, logout
 
 
 def register(request):
     form = RegisterForm()
+    form_action = reverse('contact:register')
 
     if request.method == 'POST':
         form = RegisterForm(request.POST)
@@ -14,7 +18,34 @@ def register(request):
             return redirect('contact:index')
 
     context = {
-        'form': form
+        'form': form,
+        'form_action': form_action,
     }
 
     return render(request, 'contact/register.html', context)
+
+
+def login_view(request):
+    form = AuthenticationForm(request)
+    form_action = reverse('contact:login')
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            messages.success(request, 'Logado com sucesso')
+            return redirect('contact:index')
+
+    context = {
+        'form': form,
+        'form_action': form_action,
+    }
+
+    return render(request, 'contact/login.html', context)
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('contact:login')
