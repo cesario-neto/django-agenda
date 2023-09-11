d9c09a00b2631d5504b2b2cf6c5769df88914bda
Cesario_Neto <cesarioneto7@gmail.com>
2023-09-11 09:08:00 -0300

    Creating protections for contact changes

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -14,7 +14,9 @@ def create(request):
         form = ContactForms(request.POST, request.FILES)
 
         if form.is_valid():
-            contact_form = form.save()
+            contact_form = form.save(commit=False)
+            contact_form.owner = request.user
+            contact_form.save()
             return redirect('contact:update', contact_id=contact_form.pk)
 
     context = {
@@ -27,7 +29,8 @@ def create(request):
 
 @login_required(login_url='contact:login')
 def update(request, contact_id):
-    contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    contact = get_object_or_404(
+        Contact, pk=contact_id, show=True, owner=request.user)
     form_url = reverse('contact:update', args=(contact_id,))
     form = ContactForms(instance=contact)
 
@@ -48,7 +51,8 @@ def update(request, contact_id):
 
 @login_required(login_url='contact:login')
 def delete(request, contact_id):
-    contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    contact = get_object_or_404(
+        Contact, pk=contact_id, show=True, owner=request.user)
     confirmation = request.POST.get('confirmation', 'no')
 
     if confirmation == 'yes':
